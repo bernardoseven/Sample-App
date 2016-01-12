1eac88a97b51471cfea5748bf864e50d892bb8ed
bernardoseven <bernardoseven@gmail.com>
2016-01-12 22:17:21 +0000

    Added test to ensure the route for home.html.erb and his title exists, and some code in that template with ruby functions like provide and yield, also modified application.html.erb

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ For learning purpose...
 To Start:
 - I need to think the database model and relationships and how this is going
 to be done for the most exact copy y can do in the level i'm currently programming.
+- Setting up Tasks:
+- 
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,9 @@ class PagesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Should get home" do
+    get :home
+    assert_response :success # Is a representation of a 200 http ok response
+    assert_select "title", "Home"
+  end
 end
