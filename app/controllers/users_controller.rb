2e028b8da42c827c2306b921e2b50935fc4606d3
bernardoseven <bernardoseven@gmail.com>
2016-01-14 19:51:10 +0000

    added show method to users controller, retrieving user by id, added a show.html.erb file to the users view and resources :users in routes.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id]) # we retrieve the user from the database by id.
+  end
+  
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'pages#home'
+  
+  resources :users # this line goal is to make the users resource a restful one,
+  # this means, we can post, get, patch and delete users.
+  # **************************************
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
