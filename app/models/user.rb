07a98ba1d32aafd46a7bc985ed0ae8ab568570c0
bernardoseven <bernardoseven@gmail.com>
2016-01-15 16:19:44 +0000

    added some code in user model to make a test(not finished) that asures an user already exists in the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ActiveRecord::Base
       # when the password is correct.
       validates :password, presence: true, length: { minimum: 6 } # validates password
       # presence and length.
+      # Returns the hash digest of the given string.
+      def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+      end
 end
