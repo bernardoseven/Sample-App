c08dee08e5fd6b1d6ce855ece57ac0d6b9548b81
bernardoseven <bernardoseven@gmail.com>
2016-01-15 13:22:38 +0000

    reorganizying code, touch _footer.html.erb, added routes to the routes.rb file and maked some changes to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'signup' => 'users#new'
-
   root 'pages#home'
+  get 'help' => 'pages#help'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
