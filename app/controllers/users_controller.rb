957661227e1336745409f380c0a0866b537f9c88
bernardoseven <bernardoseven@gmail.com>
2016-01-21 21:39:37 +0000

    generated Microposts controller, added @microposts variable to the users controller and then added code in the show page of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id]) # we retrieve the user from the database by id.
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
