f102e3e4448cf24999bb1e40cbe6616770a0f3bd
bernardoseven <bernardoseven@gmail.com>
2016-01-21 22:29:37 +0000

    added resources :microposts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # this means, we can post, get, patch and delete users.
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
   # **************************************
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
