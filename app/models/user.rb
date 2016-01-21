aa9e45a7b574d714897f37b3d886946f4b8e60b9
bernardoseven <bernardoseven@gmail.com>
2016-01-21 20:51:32 +0000

    associated micropost model with user model via belongs_to :user and user model with micropost via has_many :microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+      has_many :microposts # associates the user model with the micropost model.
       # we create an accessible attribute.
       attr_accessor :remember_token, :activation_token, :reset_token
       before_save :downcase_email # Obvious behavior.
