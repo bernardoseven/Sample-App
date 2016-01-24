225d39fb79b9a0f97dd47b3a79d6f21c46554864
bernardoseven <bernardoseven@gmail.com>
2016-01-24 20:46:06 +0000

    added has_many :following, through: :active_relationships, source: :followed and the correspondent methods 'follow(other_user), unfollow(other_user) and following?(other_user)'

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
       has_many :active_relationships, class_name: "Relationship",
                                       foreign_key: "follower_id",
                                       dependent: :destroy
+      has_many :following, through: :active_relationships, source: :followed
       # we create an accessible attribute.
       attr_accessor :remember_token, :activation_token, :reset_token
       before_save :downcase_email # Obvious behavior.
@@ -85,6 +86,18 @@ class User < ActiveRecord::Base
       # See "Following users" for the full implementation.
       def feed
         Micropost.where("user_id = ?", id) # this is equal to the variable microposts.
+      end
+      # Follows a user.
+      def follow(other_user)
+        active_relationships.create(followed_id: other_user.id)
+      end
+      # Unfollows a user.
+      def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+      end
+      # Returns true if the current user is following the other user.
+      def following?(other_user)
+        following.include?(other_user)
       end
       
       private
