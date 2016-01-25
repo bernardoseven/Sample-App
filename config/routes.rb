edccf76c11cc8211ff0c9e5e617f9328052b20c6
bernardoseven <bernardoseven@gmail.com>
2016-01-25 21:48:53 +0000

    added bunch of files to make posible user follow/unfollow, changed routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,15 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users # this line goal is to make the users resource a restful one,
-  # this means, we can post, get, patch and delete users.
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # **************************************
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
